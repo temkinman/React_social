854a8436b6db7d47288e697983c3b393d7bdeb26
Chegodar Artem <temkinman@mail.ru>
2020-02-16 00:21:37 +0300

    Lesson 21 Messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog}>
+                    <div className={s.link}>
+                        <NavLink to="/">Dima</NavLink>
+                    </div>
+                    <div className={s.link}>
+                        <NavLink to="/">Vasya</NavLink>
+                    </div>
+                    <div className={s.link}>
+                        <NavLink to="/">Petya</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>
+                    Hi!
+                </div>
+                <div className={s.dialog}>
+                    Hello my friend!
+                </div>
+                <div className={s.dialog}>
+                    Dou you speak English?
+                </div>
+            </div>
         </div>
     );
 }
